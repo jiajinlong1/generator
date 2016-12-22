6271ecb6556028f3f636e2b71dde5f5e46a839ca
jiajinlong <JJLdragon@163.com>
2016-12-22 10:59:42 +0800

    33333

--- src/main/java/generator/GitHubTest.java
+++ src/main/java/generator/GitHubTest.java
@@ -5,6 +5,7 @@ public class GitHubTest {
 	public static void main(String[] args) {
 		System.out.println("1111111111");
 		System.out.println("2222222222");
+		System.out.println("3333333333");
 		System.out.println("第一次响应");
 	}
 }
